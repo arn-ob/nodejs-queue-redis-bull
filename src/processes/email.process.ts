c46477e40ff2661ae5da84c64636c0d121fc51de
Arnob Almazee <arnobxtreme@gmail.com>
2022-03-31 10:18:38 +0600

    await and async added to the process

--- src/processes/email.process.ts
+++ src/processes/email.process.ts
@@ -1,27 +1,11 @@
 import { Job } from "bull";
-import nodemailer from "nodemailer";
+
 
 const emailProcess = async (job: Job) => {
 
-    // create reusable transporter object using the default SMTP transport
-    // const transporter = nodemailer.createTransport({
-    //     service: 'gmail',
-    //     auth: {
-    //         user: 'core.isp.mail@gmail.com',
-    //         pass: '01746110246Arn?'
-    //     }
-    // });
+    let todos  = await fetch('https://jsonplaceholder.typicode.com/todos')
 
-    // const mailOptions = {
-    //     from: ' "Core-ISP" <core.isp.mail@gmail.com>',
-    //     to: "arnobxtreme@gmail.com",
-    //     subject: "subject",
-    //     text: "text",
-    //     html: job.data
-    // };
-
-    // // send mail with defined transport object
-    // let info = await transporter.sendMail(mailOptions);
+    console.log("todos List", JSON.stringify(todos));
 
     console.log("Message sent: %s");
     // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
